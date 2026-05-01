a2f19c244090dfe850f4eb7e3c6b6f465f7f1fce
Ubaid Afzal <ubaidafzal117@gmail.com>
2026-05-01 11:53:33 +0500

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,13 +1,16 @@
 #pragma once
 #include "library.h"
 #include "Piece.h"
+
 using namespace std;
 
 class Board {
 private:
-    //COMPOSITION: The Board "has" Pieces. 
+    //COMPOSITION: The Board "has" Pieces.
     Piece* grid[8][8];
-
+    int fiftyMoveCounter;
+    map<string , int>positionhistory;
+    string positiongeneratekey(string currentplayercolor);
 public:
     Board();
     ~Board();
@@ -18,13 +21,18 @@ public:
     // Helper Engine Functions
     bool isPathClear(int startX, int startY, int endX, int endY);
     // Turn Management
-    // This function will call the Piece's isValidMove, check isPathClear, 
+    // This function will call the Piece's isValidMove, check isPathClear,
     // handle capturing, and finally swap the pointers.
     bool executeMove(int startX, int startY, int endX, int endY, string currentPlayerColor);
 
     // Game State Scanners
+    void recordPosition(string currentPlayerColor);
+    bool isThreefoldRepetition(string currentPlayerColor);
     bool isKingInCheck(string kingColor);
     bool hasLegalMoves(string kingColor);
     bool isCheckmate(string kingColor);
     bool isStalemate(string kingColor);
+    bool isDraw(string currentplayercolor);
+    bool islesspeice();
+    bool isThreefoldRepetition();
 };
